37d6fdaa4b99e25118d977434017b93e29aac81d
Vasyl Sava <Vasyard@gmail.com>
2020-07-13 19:32:58 +0300

    Add google maps api integration

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,9 +2,20 @@ const express                                           = require("express"),
       mongoose                                          = require("mongoose"),
       router                                            = express.Router(),
       Campground                                        = require("../models/campground"),
+      NodeGeocoder                                      = require('node-geocoder'),
       { checkCampgroundOwnership, isLoggedIn, isPaid }  = require("../middleware");
+
 router.use(isLoggedIn, isPaid);
 mongoose.set('useFindAndModify', false);
+ 
+const options = {
+  provider: 'google',
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+ 
+const geocoder = NodeGeocoder(options);
 
 //INDEX - show all campgrounds
 router.get("/", (req,res) => {
@@ -19,23 +30,35 @@ router.get("/", (req,res) => {
 });
 
 //CREATE - add new campground to DB
-router.post("/", (req,res) => {
-    const name = req.body.name;
-    const price = req.body.price;
-    const image = req.body.image;
-    const desc = req.body.description;
-    const author = {
+router.post("/", isLoggedIn, function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var price = req.body.price;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
         id: req.user._id,
         username: req.user.username
     }
-    const newCampground = {name: name, price: price, image: image, description: desc, author: author};
-    // Create a new campground and save to DB
-    Campground.create(newCampground, (err, newlyCreated) => {
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect("/campgrounds");
-        }
+    geocoder.geocode(req.body.location, function (err, data) {
+      if (err || !data.length) {
+        req.flash('error', 'Invalid address');
+        return res.redirect('back');
+      }
+      var lat = data[0].latitude;
+      var lng = data[0].longitude;
+      var location = data[0].formattedAddress;
+      var newCampground = {name: name, price: price, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+      // Create a new campground and save to DB
+      Campground.create(newCampground, function(err, newlyCreated){
+          if(err){
+              console.log(err);
+          } else {
+              //redirect back to campgrounds page
+              console.log(newlyCreated);
+              res.redirect("/campgrounds");
+          }
+      });
     });
 });
 
@@ -71,15 +94,25 @@ router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
 });
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", checkCampgroundOwnership, (req, res) => {
-    //find and update the correct campground
-    //redirect somewhere (show page)
-    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
-        if(err){
-            res.redirect("/campgrounds");
-        } else {
-            res.redirect(`/campgrounds/${req.params.id}`);
-        }
+router.put("/:id", checkCampgroundOwnership, function(req, res){
+    geocoder.geocode(req.body.location, function (err, data) {
+      if (err || !data.length) {
+        req.flash('error', 'Invalid address');
+        return res.redirect('back');
+      }
+      req.body.campground.lat = data[0].latitude;
+      req.body.campground.lng = data[0].longitude;
+      req.body.campground.location = data[0].formattedAddress;
+  
+      Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, campground){
+          if(err){
+              req.flash("error", err.message);
+              res.redirect("back");
+          } else {
+              req.flash("success","Successfully Updated!");
+              res.redirect("/campgrounds/" + campground._id);
+          }
+      });
     });
 });
 
